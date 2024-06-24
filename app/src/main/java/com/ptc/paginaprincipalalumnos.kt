7e3d2caa2ee6ad9a54d664db860f1ac28d98ba25
Levi Saravia <20230170@ricaldone.edu.sv>
2024-06-24 11:37:32 -1200

    Cambios por la pantalla de login

--- app/src/main/java/com/ptc/paginaprincipalalumnos.kt
+++ app/src/main/java/com/ptc/paginaprincipalalumnos.kt
@@ -10,7 +10,7 @@ class paginaprincipalalumnos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.`pantallainicialalumnos.xml`)
+        setContentView(R.layout.`login`)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
