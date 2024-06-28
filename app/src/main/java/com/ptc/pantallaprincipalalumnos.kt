b6bb312b413102a9cd23dee4b7cc6950a87be897
Levi Saravia <20230170@ricaldone.edu.sv>
2024-06-27 21:23:01 -0600

    pantalla principal alumnos

---
+++ app/src/main/java/com/ptc/pantallaprincipalalumnos.kt
@@ -0,0 +1,35 @@
+package com.ptc
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ptc.databinding.PantallaprincipalalumnosBinding
+
+class pantallaprincipalalumnos : AppCompatActivity() {
+
+    private lateinit var binding: PantallaprincipalalumnosBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = PantallaprincipalalumnosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_pantallaprincipalalumnos)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
